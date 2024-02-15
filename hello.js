0cd9607776dab2cdcc04697d69533ed5ac71a5a9
IsaacMwalimu <isaacmwalimu269@gmail.com>
2024-02-15 11:04:35 +0300

    first commit

---
+++ hello.js
@@ -0,0 +1,24 @@
+var myName = "isaac";
+var Myname = "isaac";
+
+//this is a single line comment//
+
+/*this is a block comment*/
+
+// var
+var myName = "joe";
+var myAge = "50";
+var mySchool = "STRT";
+var myGender = "male";
+
+// let
+let boys = 20;
+let age = 30;
+let chicken = 40;
+let money = 10;
+
+// const
+const pi = 3.142;
+const sirName = "Chris";
+const sh = "ksh";
+const phone = "Kjy";
